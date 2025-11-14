b53e40cba32bb30a341f0e4e634c735ed210f3ca
Fliperdos <86965770+Fliperdos@users.noreply.github.com>
2025-11-14 17:03:25 +0100

    Changed code formatting a bit + overall look of the code

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import time
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
 from scanner import scan_i2c
+
 displayio.release_displays()
 
 SPI = busio.SPI(clock=board.GP2, MOSI=board.GP3)
@@ -16,53 +17,45 @@ scan_i2c(I2C)
 
 ROWS = (board.GP16,)
 COLS = (board.GP17, board.GP18, board.GP19, board.GP20,)
-KEYCODES = (
-    'calc', Keycode.KEYPAD_ONE, Keycode.KEYPAD_TWO, Keycode.ENTER
-)
+KEYCODES = ('switch', Keycode.KEYPAD_ONE, Keycode.KEYPAD_TWO, Keycode.ENTER)
 
-matrix = keypad.KeyMatrix(
-    row_pins=ROWS,
-    column_pins=COLS
-)
-
+matrix = keypad.KeyMatrix(row_pins=ROWS, column_pins=COLS)
 keeb = Keyboard(usb_hid.devices)
 display = Display(SPI, I2C)
 
 lt = time.monotonic()
-def base_interrupt():
+
+def base_interrupt() -> None:           # Sensor update in Base mode
     global lt
     ct = time.monotonic()
 
-    if ct - lt > 1:
-        #print('main')
-        display.base.main()
+    if ct-lt > 3:
+        display.base.update()
         lt = ct
 
-#   Handle Key Press (event)
-def keyboard_interrupt(pos):
-    if event.pressed:
-        if display.calc.state:
-            display.calc.main(pos)
+def keyboard_interrupt(pos) -> None:    # Handles button press
+    keycode = KEYCODES[pos]
 
-        else:
-            if KEYCODES[pos] != 'calc':
-                keeb.press(KEYCODES[pos])
-            else:
-                display.switch()
-            
-    elif event.released and KEYCODES[pos] != 'calc':
-        keeb.release(KEYCODES[pos])
+    if keycode != 'switch':     # Normal button press
+        if event.pressed: keeb.press(keycode)   # button = pressed
+        else: keeb.release(keycode)     # button = released
+    
+    else:                       # Button switches display state ==> keycode == 'switch'
+        if event.pressed: display.switch()  # button = pressed
+        else: return    # button = released
 
-#   Main Loop   
-while True:
+
+while True:         # Main loop
+    if display.base.state: base_interrupt() # Sensor update if display is in Base mode
+
     event = matrix.events.get()
 
-    if display.base.state:
-        base_interrupt()
-
-    if event:
+    if event:       # if theres a button press
         pos = event.key_number
-        keyboard_interrupt(pos)
+
+        if event.pressed & display.calc.state: display.calc.main(pos)   # If display is in calculator mode
+        else: keyboard_interrupt(pos)   # If display is in base mode
+
 
 
 
