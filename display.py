4702c1efecdb5d516e6574cc6ef801041ef3676c
Fliperdos <86965770+Fliperdos@users.noreply.github.com>
2025-11-14 17:14:16 +0100

    simplified code for better readability

--- display.py
+++ display.py
@@ -1,28 +1,34 @@
 import board
-import busio
 import displayio
 import terminalio
 from adafruit_display_text import label
 from fourwire import FourWire
 from adafruit_st7735r import ST7735R
 import adafruit_ahtx0
-import adafruit_bmp280
 
 class Display:
     def __init__(self, spi, i2c):
-        self.db = FourWire(spi, command = board.GP5,
-                           chip_select = board.GP6, 
-                           reset = board.GP4)
-        self.display = ST7735R(self.db, width=160, height=128, rotation=90, bgr=True)
+        self.db = FourWire(
+            spi, command = board.GP5, chip_select = board.GP6, reset = board.GP4
+        )
+        self.display = ST7735R(
+            self.db, width=160, height=128, rotation=90, bgr=True
+        )
 
         self.base = Base(i2c, self)
         self.calc = Calculator(self)
 
+        self.display.root_group = self.base.group
+
     def switch(self):
+        """switches between modes, clears calulator inputs when turning off calculator mode"""
         self.base.toggle()
         self.calc.toggle()
 
-    def call(self, group):
+        if not self.calc.state: self.calc.reset()
+
+    def set_display(self, group):
+        """gets the modes displayio group and sets it as the root group"""
         self.display.root_group = group
 
 
@@ -33,25 +39,29 @@ class Base(Display):
         self.aht = adafruit_ahtx0.AHTx0(i2c)
         self.group = displayio.Group()
 
-        self.temp = label.Label(font=terminalio.FONT,
-                           text='',
-                           x=5, y=30, color=0x0062ff)
-        self.hum = label.Label(font=terminalio.FONT,
-                           text='',
-                           x=5, y=45, color=0x0062ff)
+        self.temp = label.Label(
+            font=terminalio.FONT, x=5, y=30, color=0x0062ff,
+            text=f'AHT temperature: {self.aht.temperature}'
+        )
+        self.hum = label.Label(
+            font=terminalio.FONT, x=5, y=40, color=0x0062ff,
+            text=f'AHT humidity: {self.aht.relative_humidity}'
+        )
         
         self.group.append(self.temp)
         self.group.append(self.hum)
 
     def toggle(self):
+        """toggle mode state"""
         self.state = not self.state
-        print(f'- Base {self.state}')
+        if self.state: self.parent.set_display(self.group)
 
-        if self.state: self.parent.call(self.group)
-
-    def main(self):
+    def update(self):
+        """updates sensor values"""
         self.temp.text = f'AHT temperature: {self.aht.temperature}'
         self.hum.text = f'AHT humidity: {self.aht.relative_humidity}'
+
+
 
 class Calculator(Display):
     def __init__(self, parent):
@@ -63,38 +73,40 @@ class Calculator(Display):
                               x=5, y=40, color=0x0062ff)
         self.result = label.Label(font=terminalio.FONT, text='',
                                   x=5, y=50, color=0x0062ff)
-        self.txt = label.Label(font=terminalio.FONT, text='CALC ON',   
+        self.title = label.Label(font=terminalio.FONT, text='CALC ON',   
                               x=5, y=20, color=0x0062ff)
-        self.group.append(self.txt)
+
+        self.group.append(self.title)
         self.group.append(self.eq)
         self.group.append(self.result)
         
     
     def toggle(self):
+        """toggle mode state"""
         self.state = not self.state
-        print(f'- Calc {self.state}')
 
-        if self.state:
-            self.eq.text = ''
-            self.result.text = ''
-            self.parent.call(self.group)
+        if self.state: 
+            self.parent.set_display(self.group)
+
+    def reset(self):
+        """clears user inputs"""
+        self.eq.text = ''
+        self.result.text = ''
 
     def main(self, pos):
-        if pos == 0:
-            self.parent.switch()
+        """calculators main user input logic"""
+        if pos == 0: self.parent.switch()
 
-        if pos == 1:
-            self.eq.text += '1'
-            print(self.eq.text)
+        elif pos == 1: self.eq.text += '1'
 
-        if pos == 2:
-            self.eq.text += '+'
-            print(self.eq.text)
+        elif pos == 2: self.eq.text += '+'
 
-        if pos == 3:
+        elif pos == 3:
             try:
                 x = eval(self.eq.text)  
                 self.result.text = f'Result: {x}'
+                self.eq.text = ''
+
             except Exception as e: self.result.text = f'{e}'
 
 
